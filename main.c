d30865689531cd02020a526209542797b8742697
N1KUSO <kisilnikolay1356@gmail.com>
2024-05-06 00:06:44 +0300

    task 2

--- main.c
+++ main.c
@@ -3,7 +3,19 @@
 #define MAX_SIZE 100
 
 void task2() {
+    FILE *input_file = fopen("../input.txt", "r");
+    FILE *temp_file = fopen("../temp.txt", "w");
 
+    float num;
+    while(fscanf(input_file, "%f", &num) == 1) {
+        fprintf(temp_file, "%.2f ", num);
+    }
+
+    fclose(input_file);
+    fclose(temp_file);
+
+    remove("../input.txt");
+    rename("../temp.txt", "../input.txt");
 }
 
 void task1() {
@@ -36,7 +48,7 @@ void task1() {
 }
 
 int main() {
-    task1();
+    task2();
 
     return 0;
 }
