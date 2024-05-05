a654aa0db3b6a6f07a9a66b1f0eff32ed2325f00
N1KUSO <kisilnikolay1356@gmail.com>
2024-05-06 00:02:01 +0300

    task 1

--- main.c
+++ main.c
@@ -1,6 +1,42 @@
 #include <stdio.h>
 
+#define MAX_SIZE 100
+
+void task2() {
+
+}
+
+void task1() {
+    FILE *input_file = fopen("../input.txt", "r");
+    FILE *temp_file = fopen("../temp.txt", "w");
+
+    int n;
+    while (fscanf(input_file, "%d", &n) == 1) {
+        int matrix[MAX_SIZE][MAX_SIZE];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                fscanf(input_file, "%d", &matrix[i][j]);
+            }
+        }
+
+        for (int j = 0; j < n; j++) {
+            for (int i = 0; i < n; i++) {
+                fprintf(temp_file, "%d ", matrix[i][j]);
+            }
+            fprintf(temp_file, "\n");
+        }
+    }
+
+    fclose(input_file);
+    fclose(temp_file);
+
+    remove("../input.txt");
+    rename("../temp.txt", "../input.txt");
+}
+
 int main() {
-    printf("Hello, World!\n");
+    task1();
+
     return 0;
 }
