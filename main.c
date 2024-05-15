1493c98eec73a6b61b37f3a815de695e199b6e9e
N1KUSO <kisilnikolay1356@gmail.com>
2024-05-15 17:16:54 +0300

    task 7 + test

--- main.c
+++ main.c
@@ -45,136 +45,6 @@ void get_longest_word(char *line, char* longest_word) {
     strcpy(longest_word, max_word);
 }
 
-int isSymmetric(int **matrix, int n) {
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (matrix[i][j] != matrix[j][i]) {
-                return 0;
-            }
-        }
-    }
-    return 1;
-}
-
-void transpose(int **matrix, int n) {
-    int temp;
-    for (int i = 0; i < n; i++) {
-        for (int j = i + 1; j < n; j++) {
-            temp = matrix[i][j];
-            matrix[i][j] = matrix[j][i];
-            matrix[j][i] = temp;
-        }
-    }
-}
-
-void task8() {
-    FILE *input_file = fopen("../input.bin", "rb");
-    FILE *temp_file = fopen("../temp.bin", "wb");
-
-    int n = fread(&n, sizeof(int), 1, input_file);
-
-    int **matrix = (int **)malloc(n * sizeof(int *));
-    for (int i = 0; i < n; i++) {
-        matrix[i] = (int *)malloc(n * sizeof(int));
-    }
-
-    while (fread(matrix[0], sizeof(int), n * n, input_file) != 0) {
-        if (!isSymmetric(matrix, n)) {
-            transpose(matrix, n);
-        }
-        fwrite(matrix[0], sizeof(int), n * n, temp_file);
-    }
-
-    fclose(input_file);
-    fclose(temp_file);
-
-    for (int i = 0; i < n; i++) {
-        free(matrix[i]);
-    }
-    free(matrix);
-
-    remove("../input.bin");
-    rename("../temp.bin", "../input.bin");
-}
-
-void task7() {
-    FILE *input_file = fopen("../input.bin", "rb");
-    FILE *temp_file = fopen("../temp.bin", "wb");
-
-    int number;
-    while (fread(&number, sizeof(int), 1, input_file) != 0) {
-        if (number >= 0) {
-            fwrite(&number, sizeof(int), 1, temp_file);
-        }
-    }
-
-    rewind(input_file);
-
-    while (fread(&number, sizeof(int), 1, input_file) != 0) {
-        if (number < 0) {
-            fwrite(&number, sizeof(int), 1, temp_file);
-        }
-    }
-
-    fclose(input_file);
-    fclose(temp_file);
-
-    remove("../input.bin");
-    rename("../temp.bin", "../input.bin");
-}
-
-void task6() {
-    FILE *input_file = fopen("../input.bin", "rb");
-    FILE *output_file = fopen("../output.bin", "wb");
-    
-    double x = 2.0;
-
-    while (!feof(input_file)) {
-        int term_count;
-        if (fread(&term_count, sizeof(int), 1, input_file) != 1)
-            break;
-
-        Polynomial *polys = (Polynomial *)malloc(term_count * sizeof(Polynomial));
-        fread(polys, sizeof(Polynomial), term_count, input_file);
-
-        double sum = 0.0;
-        for (int i = 0; i < term_count; ++i) {
-            sum += polys[i].coefficient * pow(x, polys[i].power);
-        }
-
-        if (sum != 0) {
-            fwrite(&term_count, sizeof(int), 1, output_file);
-            fwrite(polys, sizeof(Polynomial), term_count, output_file);
-        }
-
-        free(polys);
-    }
-
-    fclose(input_file);
-    fclose(output_file);
-}
-
-void generate_input_output_task6() {
-    FILE *input_file = fopen("../input.bin", "wb");
-    FILE *excepted_file = fopen("../excepted.bin", "wb");
-
-    int power = 3;
-    Polynomial poly1[] = {{2, 3}, {1, -12}, {0, 12}};
-    fwrite(&power, sizeof(int), 1, input_file);
-    fwrite(poly1, sizeof(Polynomial), power, input_file);
-
-    power = 4;
-    Polynomial poly2[] = {{3, 14}, {2, -24}, {1, 48}, {0, -32}};
-    fwrite(&power, sizeof(int), 1, input_file);
-    fwrite(poly2, sizeof(Polynomial), power, input_file);
-
-    fwrite(&power, sizeof(int), 1, excepted_file);
-    fwrite(poly2, sizeof(Polynomial), power, excepted_file);
-
-    fclose(input_file);
-    fclose(input_file);
-}
-
 void task5() {
     FILE *input_file = fopen("../input.txt", "r");
     FILE *temp_file = fopen("../temp.txt", "w");
@@ -279,6 +149,58 @@ void task1() {
     rename("../temp.txt", "../input.txt");
 }
 
+void task6() {
+    FILE *input_file = fopen("../input.bin", "rb");
+    FILE *output_file = fopen("../output.bin", "wb");
+    
+    double x = 2.0;
+
+    while (!feof(input_file)) {
+        int term_count;
+        if (fread(&term_count, sizeof(int), 1, input_file) != 1)
+            break;
+
+        Polynomial *polys = (Polynomial *)malloc(term_count * sizeof(Polynomial));
+        fread(polys, sizeof(Polynomial), term_count, input_file);
+
+        double sum = 0.0;
+        for (int i = 0; i < term_count; ++i) {
+            sum += polys[i].coefficient * pow(x, polys[i].power);
+        }
+
+        if (sum != 0) {
+            fwrite(&term_count, sizeof(int), 1, output_file);
+            fwrite(polys, sizeof(Polynomial), term_count, output_file);
+        }
+
+        free(polys);
+    }
+
+    fclose(input_file);
+    fclose(output_file);
+}
+
+void generate_input_output_task6() {
+    FILE *input_file = fopen("../input.bin", "wb");
+    FILE *excepted_file = fopen("../excepted.bin", "wb");
+
+    int power = 3;
+    Polynomial poly1[] = {{2, 3}, {1, -12}, {0, 12}};
+    fwrite(&power, sizeof(int), 1, input_file);
+    fwrite(poly1, sizeof(Polynomial), power, input_file);
+
+    power = 4;
+    Polynomial poly2[] = {{3, 14}, {2, -24}, {1, 48}, {0, -32}};
+    fwrite(&power, sizeof(int), 1, input_file);
+    fwrite(poly2, sizeof(Polynomial), power, input_file);
+
+    fwrite(&power, sizeof(int), 1, excepted_file);
+    fwrite(poly2, sizeof(Polynomial), power, excepted_file);
+
+    fclose(input_file);
+    fclose(input_file);
+}
+
 void print_polynomials(const char* filename) {
     FILE* file = fopen(filename, "rb");
 
@@ -298,6 +220,78 @@ void print_polynomials(const char* filename) {
     fclose(file);
 }
 
+void task7() {
+    FILE *input_file = fopen("../input.bin", "rb");
+    FILE *output_file = fopen("../output.bin", "wb");
+
+    fseek(input_file, 0, SEEK_END);
+    long file_size = ftell(input_file);
+    fseek(input_file, 0, SEEK_SET);
+
+    int count = file_size / sizeof(int);
+    int *numbers = (int *)malloc(file_size);
+
+    fread(numbers, sizeof(int), count, input_file);
+    fclose(input_file);
+
+    int *positive_numbers = (int *)malloc(file_size);
+    int *negative_numbers = (int *)malloc(file_size);
+    int pos_count = 0, neg_count = 0;
+
+    for (int i = 0; i < count; ++i) {
+        if (numbers[i] > 0) {
+            positive_numbers[pos_count++] = numbers[i];
+        } else {
+            negative_numbers[neg_count++] = numbers[i];
+        }
+    }
+
+    fwrite(positive_numbers, sizeof(int), pos_count, output_file);
+    fwrite(negative_numbers, sizeof(int), neg_count, output_file);
+    fclose(output_file);
+
+    free(numbers);
+    free(positive_numbers);
+    free(negative_numbers);
+}
+
+void generate_input_output_task7() {
+    FILE *input_file = fopen("../input.bin", "wb");
+    FILE *excepted_file = fopen("../excepted.bin", "wb");
+
+    int numbers[] = {1,2,-3,5,-9,8,12};
+    fwrite(numbers, sizeof(int), 1, input_file);
+
+    int exc_numbers[] = {-3,-9,1,2,5,8,12};
+    fwrite(exc_numbers, sizeof(int), 1, excepted_file);
+
+    fclose(input_file);
+    fclose(input_file);
+}
+
+void test_task7() {
+    generate_input_output_task7();
+    task7();
+
+    FILE *output_file = fopen("../output.bin", "rb");
+    FILE *excepted_file = fopen("../excepted.bin", "rb");
+
+    int num1, num2;
+    int error = 0;
+    while (fread(&num1, sizeof(int), 1, output_file) == 1 && fread(&num2, sizeof(int), 1, excepted_file) == 1) {
+        if (num1 != num2) {
+            error = 1;
+            break;
+        }
+    }
+
+    fclose(output_file);
+    fclose(excepted_file);
+
+    if (error) {
+        printf("Test failed!\n");
+    }
+}
 
 void test_task6() {
     generate_input_output_task6();
@@ -325,6 +319,7 @@ void test_task6() {
 
 int main() {
     test_task6();
+    test_task7();
 
     return 0;
 }
